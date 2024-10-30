0e2907104f50a132a433036b45f2ffccc6f900d8
Rebecka94 <rebeckalarsson.1994@gmail.com>
2024-10-30 11:17:50 +0100

    edited javascript file with new content

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
+const doorWorld = {
+    startRoom: {
+        description: "Välommen till dörrarnas värld, vill du gå igenom den första dörren?",
+        doors: {
+            doorOne: "Röda dörren",
+        }
+    },
+
+    roomOne: {
+    description: "Välommen till ängen, du kan välja att antingen gå igenom den rosa dörren eller den blåa dörren",
+    doors: {
+        doorOne: "Rosa dörren",
+        doorTwo: "Blåa dörren",
+    }
+},
+
+roomTwo: {
+    description: "Välommen till dungen, du kan välja att antingen gå igenom den gula dörren eller den gröna dörren",
+    doors: {
+        doorOne: "Gula dörren",
+        doorTwo: "Gröna dörren",
+    }
+},
+
+roomThree: {
+    description: "Välommen till träsket, du kan välja att antingen gå igenom den lila dörren eller den svarta dörren",
+    doors: {
+        doorOne: "Lila dörren",
+        doorTwo: "Svarta dörren",
+    }
+},
+};
+
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
     console.log("Start of the program");
-    setUpBtn()
-}
-
-function setUpBtn() {
-    firstBtn.onclick = openDoorBtn;
-}
-
-function openDoorBtn() {
-    document.getElementById("doorClosed").style.display = 'none';
-    document.getElementById("doorOpen").style.display = 'block';
-    document.getElementById("firstText").style.display = 'none';
-    document.getElementById("secondText").style.display = 'block';
-    document.getElementById("firstBtn").style.display = 'none';
-    document.getElementById("yesBtn").style.display = 'block';
-    document.getElementById("noBtn").style.display = 'block';
+    setUpButtons();
+    currentRoom();
 }
 
 
