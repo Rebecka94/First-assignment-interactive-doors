8db370f209c8168d30f8e17e214ba34592b54ece
Rebecka94 <rebeckalarsson.1994@gmail.com>
2024-11-11 13:45:03 +0100

    changed the js file to other functions

--- main.js
+++ main.js
@@ -1,69 +1,92 @@
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
-    console.log("Start of the program");
-    setUpButtons();
+  console.log("Start of the program");
+  loadStartScene();
 }
 
-function setUpButtons() {
-    document.getElementById("redDoor").onclick = () => openDoorFunction("red");
-    document.getElementById("blueDoor").onclick = () => openDoorFunction("blue");
-    document.getElementById("pinkDoor").onclick = () => openDoorFunction("pink");
-    document.getElementById("yellowDoor").onclick = () => openDoorFunction("yellow");
-    document.getElementById("greenDoor").onclick = () => openDoorFunction("green");
-    document.getElementById("purpleDoor").onclick = () => openDoorFunction("purple");
-    document.getElementById("brownDoor").onclick = () => openDoorFunction("brown");
-};
+function loadStartScene() {
+  roomContainer.innerText = "";
 
-function openDoorFunction(color) {
-    const rumBeskrivning = document.getElementById("roomDescription");
-    document.getElementById("mainFirst").style.display = "none";
+  const headingText = document.createElement("h1");
+  headingText.textContent = "Välkommen till dörrarnas värld";
 
-    if (color === "red") {
-        document.body.style.backgroundImage = "url('Assets/swamp.webp')";
-        rumBeskrivning.textContent = "Du har hamnat i träsket.. hmm.. välj en ny dörr för att komma till en ny värld"
-        
-        document.getElementById("firstRoom").style.display = "flex";
-        document.getElementById("startingRoom").style.display = "none";
-    } else if (color === "blue") {
-        document.body.style.backgroundImage = "url('Assets/Dungen.jpg')";
-        rumBeskrivning.textContent = "Du har hamnat vid dungen."
-        
-        document.getElementById("thirdRoom").style.display = "flex";
-        document.getElementById("firstRoom").style.display = "none";
-    }
-    else if (color === "pink") {
-        document.body.style.backgroundImage = "url('Assets/Grottan.jpg')";
-        rumBeskrivning.textContent = "Åh nej.. Du öppnade dörren till grottan. Skynda dig här ifrån så att trollet inte äter upp dig"
-        
-        document.getElementById("secondRoom").style.display = "flex";
-        document.getElementById("firstRoom").style.display = "none";
-    }
-    else if (color === "yellow") {
-        document.body.style.backgroundImage = "url('Assets/Forrest.webp')";
-        rumBeskrivning.textContent = "Nu är du tillbaka där vi började.. Försök igen!"
-       
-        document.getElementById("startingRoom").style.display = "flex";
-        document.getElementById("secondRoom").style.display = "none";
-    }
-    else if (color === "green") {
-        document.body.style.backgroundImage = "url('Assets/swamp.webp')";
-        rumBeskrivning.textContent = "Du har hamnat i träsket.. hmm.. välj en ny dörr för att komma till en ny värld"
-       
-        document.getElementById("firstRoom").style.display = "flex";
-        document.getElementById("secondRoom").style.display = "none";
-    }
-    else if (color === "purple") {
-        document.body.style.backgroundImage = "url('Assets/Grottan.jpg')";
-        rumBeskrivning.textContent = "Åh nej.. Du öppnade dörren till grottan. Skynda dig här ifrån så att trollet inte äter upp dig"
-        
-        document.getElementById("secondRoom").style.display = "flex";
-        document.getElementById("thirdRoom").style.display = "none";
-    }
-    if (color === "brown") {
-        rumBeskrivning.textContent = "Grattis. Du tog dig i mål"
+  const pText = document.createElement("p");
+  pText.textContent =
+    "I den här världen kommer du gå igenom ett antal dörrar. Vissa dörrar tar dig till helt fantastiska ställen medans andra kommer att få dig att tvivla på dina val. Tryck på den första dörren för att starta spelet";
 
-        document.getElementById("endingRoom").style.display = "flex";
-        document.getElementById("thirdRoom").style.display = "none";
-    }
+  const button = document.createElement("button");
+  button.textContent = "Börja";
+  button.onclick = loadFirstRoom;
+
+  roomContainer.append(headingText, pText, button);
+}
+
+function loadFirstRoom() {
+  document.body.style.backgroundColor = "pink";
+  roomContainer.innerText = "";
+  const headingText = document.createElement("h1");
+  headingText.textContent = "ojojoj";
+
+  const pText = document.createElement("p");
+  pText.textContent = "hejhejhej";
+
+  roomContainer.append(headingText, pText);
+
+  doorContainer.innerText = "";
+  const image1 = document.createElement("img");
+  image1.src = "Assets/Red door.png";
+  const image2 = document.createElement("img");
+  image2.src = "Assets/blue door.png";
+
+  doorContainer.append(image1, image2);
+
+  buttonContainer.innerText = "";
+  const button1 = document.createElement("button");
+  button1.textContent = "Röda dörren";
+  button1.onclick = loadSecondRoom;
+
+  const button2 = document.createElement("button");
+  button2.textContent = "Blåa dörren";
+  button2.onclick = loadThirdRoom;
+
+  buttonContainer.append(button1, button2);
+}
+
+function loadSecondRoom() {
+  document.body.style.backgroundColor = "yellow";
+
+  roomContainer.innerText = "";
+  const headingText = document.createElement("h1");
+  headingText.textContent = "Välkommen till det gula rummet";
+
+  const pText = document.createElement("p");
+  pText.textContent = "Vilken dörr ska du gå igenom härnäst?";
+  roomContainer.append(headingText, pText);
+
+  doorContainer.innerText = "";
+  const image1 = document.createElement("img");
+  image1.src = "Assets/Green door.png";
+  const image2 = document.createElement("img");
+  image2.src = "Assets/pink door.png";
+  doorContainer.append(image1, image2);
+
+  buttonContainer.innerText = "";
+  const button1 = document.createElement("button");
+  button1.textContent = "Gröna dörren";
+  button1.onclick = loadFirstRoom;
+
+  const button2 = document.createElement("button");
+  button2.textContent = "Rosa dörren";
+  button2.onclick = loadThirdRoom;
+
+  buttonContainer.append(button1, button2);
+}
+
+function loadThirdRoom() {
+  document.body.style.backgroundColor = "red";
+
+  roomContainer.innerText = "";
+  doorContainer.innerText = "";
+  buttonContainer.innerText = "";
 }
