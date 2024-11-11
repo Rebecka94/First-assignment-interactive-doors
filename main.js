18125cfc2cbe9979d07179ef947b48d3020562dc
Rebecka94 <rebeckalarsson.1994@gmail.com>
2024-11-11 14:04:45 +0100

    added more functions

--- main.js
+++ main.js
@@ -87,6 +87,67 @@ function loadThirdRoom() {
   document.body.style.backgroundColor = "red";
 
   roomContainer.innerText = "";
+  const headingText = document.createElement("h1");
+  headingText.textContent = "Välkommen till det röda rummet";
+
+  const pText = document.createElement("p");
+  pText.textContent = "Vilken dörr ska du gå igenom härnäst?";
+  roomContainer.append(headingText, pText);
+
+  doorContainer.innerText = "";
+  const image1 = document.createElement("img");
+  image1.src = "Assets/Purple door.png";
+  const image2 = document.createElement("img");
+  image2.src = "Assets/Brown door.png";
+  doorContainer.append(image1, image2);
+
+  buttonContainer.innerText = "";
+  const button1 = document.createElement("button");
+  button1.textContent = "Lila dörren";
+  button1.onclick = loadSecondRoom;
+
+  const button2 = document.createElement("button");
+  button2.textContent = "Bruna dörren";
+  button2.onclick = loadFourthRoom;
+
+  buttonContainer.append(button1, button2);
+}
+
+function loadFourthRoom() {
+  document.body.style.backgroundColor = "purple";
+
+  roomContainer.innerText = "";
+  const headingText = document.createElement("h1");
+  headingText.textContent = "Välkommen till det lila rummet";
+
+  const pText = document.createElement("p");
+  pText.textContent = "Du är nu i vid sista dörren. Men har du nyckeln för att kunna gå igenom?";
+  roomContainer.append(headingText, pText);
+
+  doorContainer.innerText = "";
+  const image1 = document.createElement("img");
+  image1.src = "Assets/Yellow door.png";
+  doorContainer.append(image1);
+
+  buttonContainer.innerText = "";
+  const button1 = document.createElement("button");
+  button1.textContent = "Gula dörren";
+  button1.onclick = loadFinalRoom;
+
+  buttonContainer.append(button1);
+}
+
+function loadFinalRoom() {
+  document.body.style.backgroundColor = "orange";
+
+  roomContainer.innerText = "";
+  const headingText = document.createElement("h1");
+  headingText.textContent = "Välkommen till det sista rummet";
+
+  const pText = document.createElement("p");
+  pText.textContent = "Grattis, du tog dig i mål!";
+  roomContainer.append(headingText, pText);
+
   doorContainer.innerText = "";
   buttonContainer.innerText = "";
 }
